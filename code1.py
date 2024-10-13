ccca2e42055fd97eff845435632bd20e356cdcf8
ariki4160 <mailto:577649+ariki4160@users.noreply.github.com>
2024-10-14 00:01:44 +0900

    上旬の表示

--- code1.py
+++ code1.py
@@ -1,1 +1,10 @@
-print('hello world')
+import datetime
+
+def main():
+    now_day =datetime.datetime.now().day
+
+    if now_day <= 10:
+        print('上旬です')
+
+if __name__ == '__main__':
+    main()
